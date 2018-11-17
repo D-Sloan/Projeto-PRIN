7d96f33d0fbdb1ec9f7bf8ab80a85a1a07936dd4
Davyd <dsss1@ifal.edu.br>
2018-11-17 03:00:43 -0200

    17/11

--- PRIN/teste.php
+++ PRIN/teste.php
@@ -9,6 +9,7 @@
 	$conn = mysqli_connect("localhost", "root", "", "teste");
 	mysqli_query($conn,"CREATE TABLE nomes (nome varchar(50));");
 	mysqli_query($conn,"INSERT INTO nomes (nome) VALUES('$Name')");
+	mysqli_close($conn);
 
 
 
